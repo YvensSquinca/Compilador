a6a0abb548219dd096c48c1ed2d8b9da72781d4b
Yvens Squinca <43186037+YvensSquinca@users.noreply.github.com>
2021-09-17 13:57:32 -0300

    Add files via upload

---
+++ compilador.py
@@ -0,0 +1,19 @@
+import os
+from cx_Freeze import setup, Executable
+
+os.environ['TCL_LIBRARY'] = "C:\\Users\\SEUUSUARIO\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6" #Carrega as bibliotecas
+os.environ['TK_LIBRARY'] = "C:\\Users\\DELL\\SEUUSUARIO\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6" #Carrega as bibliotecas
+
+setup(
+    name = "Programa", #Nome do Programa
+    version = "1.0.0", #Versão
+    options = {"build_exe":{
+        'packages': ["os","sys","ctypes","win32con"],
+        #Aqui incluimos nossos arquvios, como icones e imagens
+        'include_files': ['C:\\Users\\SEUUSUARIO\\AppData\\Local\\Programs\\Python\\Python36-32\\DLLs\\tcl86t.dll','C:\\Users\\DELL\\AppData\\Local\\Programs\\Python\\Python36-32\\DLLs\\tk86t.dll', #Essas 2 DLLs são importantes
+        'transfer.png','transfer.ico','verde.png','vermelho.png','cinza.png'], #Alguns exemplos de Imagens e icones
+        'include_msvcr': True,
+        },
+    },
+    executables = [Executable("NomedoScript.py",base="Win32GUI",icon="NomedoIcone.ico")] #Nome do Script que sera compilado.
+    )
